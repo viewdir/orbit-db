8839fb4a59c631aef72f6bb3fd510290c332790e
aeschylus <scipioaffricanus@gmail.com>
2017-04-02 16:43:49 -0700

    initial hacking

--- examples/browser/index.js
+++ examples/browser/index.js
@@ -13,7 +13,7 @@ const openDatabase = () => {
 
   const dbname = dbnameField.value
   const username = new Date().getTime()
-  const key = 'greeting'
+  const key = 'textArea'
 
   const ipfs = new IPFS({
     IpfsDataDir: '/orbit-db-/examples/browser',
@@ -22,7 +22,7 @@ const openDatabase = () => {
 
   function handleError(e) {
     console.error(e.stack)
-    elm.innerHTML = e.message  
+    elm.innerHTML = e.message
   }
 
   ipfs.on('error', (e) => handleError(e))
@@ -33,77 +33,43 @@ const openDatabase = () => {
     const orbit = new OrbitDB(ipfs, username)
 
     const db = orbit.kvstore(dbname, { maxHistory: 5, syncHistory: false, cachePath: '/orbit-db' })
-    const log = orbit.eventlog(dbname + ".log", { maxHistory: 5, syncHistory: false, cachePath: '/orbit-db' })
-    const counter = orbit.counter(dbname + ".count", { maxHistory: 5, syncHistory: false, cachePath: '/orbit-db' })
 
-    const creatures = ['👻', '🐙', '🐷', '🐬', '🐞', '🐈', '🙉', '🐸', '🐓']
-    const idx = Math.floor(Math.random() * creatures.length)
-    const creature = creatures[idx]
-
-    const interval = Math.floor((Math.random() * 5000) + 3000)
-
-    let count = 0
-    const query = () => {
-      const value = "GrEEtinGs from " + username + " " + creature + ": Hello #" + count + " (" + new Date().getTime() + ")"
+    const query = (value) => {
       // Set a key-value pair
-      count ++
       db.put(key, value)
-        .then(() => counter.inc()) // Increase the counter by one
-        .then(() => log.add(value)) // Add an event to 'latest visitors' log
         .then(() => getData())
         .catch((e) => handleError(e))
     }
 
     const getData = () => {
       const result = db.get(key)
-      const latest = log.iterator({ limit: 5 }).collect()
-      const count  = counter.value
 
       ipfs.pubsub.peers(dbname + ".log")
         .then((peers) => {
           const output = `
-            <b>You are:</b> ${username} ${creature}<br>
+            <b>You are:</b> ${username}<br>
             <b>Peers:</b> ${peers.length}<br>
             <b>Database:</b> ${dbname}<br>
-            <br><b>Writing to database every ${interval} milliseconds...</b><br><br>
-            <b>Key-Value Store</b>
-            -------------------------------------------------------
-            Key | Value
-            -------------------------------------------------------
+            <br><b>Writing to database on keyup</b><br><br>
             ${key} | ${result}
-            -------------------------------------------------------
-
-            <b>Eventlog</b>
-            -------------------------------------------------------
-            Latest Updates
-            -------------------------------------------------------
-            ${latest.reverse().map((e) => e.payload.value).join('\n')}
-
-            <b>Counter</b>
-            -------------------------------------------------------
-            Visitor Count: ${count}
-            -------------------------------------------------------
             `
           elm.innerHTML = output.split("\n").join("<br>")
+          document.querySelector('textarea').value = result;
         })
     }
 
-    db.events.on('synced', () => getData())
-    counter.events.on('synced', () => getData())
-    log.events.on('synced', () => getData())
+    // db.events.on('synced', () => getData())
 
-    db.events.on('ready', () => getData())
-    counter.events.on('ready', () => getData())
-    log.events.on('ready', () => getData())
+    // db.events.on('ready', () => getData())
 
     // Start query loop when the databse has loaded its history
-    db.load(10)
-      .then(() => counter.load(10))
-      .then(() => log.load(10))
-      .then(() => {
-        count = counter.value
-        setInterval(query, interval)
-      })
+    // db.load(5)
+    //   .then(() => {
+    //     setInterval(query, interval)
+    //   })
+    document.querySelector('textarea').addEventListener('keyup', (event) => {
+      query(document.querySelector('textarea').value)
+    })
   })
 }
 
